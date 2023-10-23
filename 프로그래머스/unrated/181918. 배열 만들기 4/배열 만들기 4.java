30fc3612a4d9301d3fb1346d330eef3f30ec1636
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2023-10-23 14:33:26 +0900

    [level unrated] Title: 배열 만들기 4, Time: 6.78 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> stk = new ArrayList<>();
+        for(int i = 0; i < arr.length;) {
+            if(stk.size() == 0) {
+                stk.add(arr[i]);
+                i++;
+            } else if (stk.size() != 0 && stk.get(stk.size() - 1) < arr[i]) {
+                stk.add(arr[i]);
+                i++;
+            } else {
+                stk.remove(stk.size() - 1);
+            }
+        }
+        return stk.stream().mapToInt(i -> i).toArray();
+    }
+}

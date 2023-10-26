a8099ef538fb22ae5d4b4630ddd3e2a5eb974ef7
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2023-10-26 13:20:41 +0900

    [level unrated] Title: 공 던지기, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120843. 공 던지기/공 던지기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] numbers, int k) {
+        int answer = 0;
+        int cnt = 0;
+        
+        
+        return numbers[2 * (k - 1) % numbers.length];
+    }
+}

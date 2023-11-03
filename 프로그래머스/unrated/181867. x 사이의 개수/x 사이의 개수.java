cd542b86b06b6db2934b44a8075d41fa7c1d4f25
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2023-11-03 10:41:57 +0900

    [level unrated] Title: x 사이의 개수, Time: 4.65 ms, Memory: 88.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String myString) {
+        
+        String[] str = myString.split("x", myString.length());
+        int[] answer = new int[str.length];
+        
+        for(int i = 0; i < str.length; i++){
+            answer[i] = str[i].length();
+        }
+        return answer;
+    }
+}

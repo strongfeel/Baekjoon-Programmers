e3e7e40d8fed1fe6d5f837bccf2d695ddfaff83a
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2023-11-06 20:58:53 +0900

    [Silver IV] Title: 동전 0, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+    	
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));       
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		                      
+		int N = Integer.parseInt(st.nextToken());
+  		int K = Integer.parseInt(st.nextToken());				
+		int[] coin = new int [N];
+        
+		for (int i = 0; i < N; i++) {        	
+			coin[i] = Integer.parseInt(br.readLine());
+		}	
+        
+		int count = 0;
+        
+		for (int i = N-1; i >= 0; i--) {
+			if(K >= coin[i]) {
+				count += K / coin[i];
+				K = K % coin[i];
+			}
+		}
+		
+		bw.write(String.valueOf(count));				
+		br.close();
+		bw.close();
+ 	}
+}

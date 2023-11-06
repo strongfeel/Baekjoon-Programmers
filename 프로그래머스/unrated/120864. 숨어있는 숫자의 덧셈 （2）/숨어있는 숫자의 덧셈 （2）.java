94ee148f5387747f8417205eabd2d7e648deda72
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2023-11-06 09:57:38 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.15 ms, Memory: 69.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String[] str = my_string.split("[a-zA-Z]");
+        for(int i = 0; i < str.length; i++) {
+            answer += !str[i].isEmpty() ? Integer.parseInt(str[i]) : 0;
+        }
+        return answer;
+    }
+}

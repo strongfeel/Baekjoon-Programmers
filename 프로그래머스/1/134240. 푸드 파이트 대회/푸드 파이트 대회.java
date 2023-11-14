c7dee3acb3a4d4eb73c1137779a9d8a3a7da3a7f
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2023-11-14 09:52:27 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 2.70 ms, Memory: 84.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 1; i < food.length; i++) {
+            int count = food[i] / 2;
+            sb.append(String.valueOf(i).repeat(count));
+        }
+        
+        answer = sb + "0";
+        answer += sb.reverse();
+        
+        return answer;
+    }
+}

d997f7051c31c367c40c0262cd97638d411d72bf
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2024-09-23 09:13:09 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.13 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -0,0 +1,21 @@
+function solution(arr) {
+    var answer = 1;
+    for(let i = 0; i < arr.length; i++) {
+        answer = lcm(answer, arr[i])
+    }
+    return answer;
+}
+
+const gcd = (a,b) => {
+    if(b === 0){
+        return a;
+    }else if(a%b === 0){
+        return b;
+    }else{
+        return gcd(b, a%b);
+    };
+};
+
+const lcm = (a,b) => {
+    return (a*b) / gcd(a,b);
+};

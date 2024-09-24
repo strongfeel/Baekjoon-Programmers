5d4b965bdd781d734da44da8ce64a28982bf762b
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2024-09-24 09:16:36 +0900

    [level 2] Title: 귤 고르기, Time: 85.37 ms, Memory: 63.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,15 @@
+function solution(k, tangerine) {
+    var answer = 0;
+    const tDict = {};
+    
+    tangerine.forEach((t) => (tDict[t] = (tDict[t] || 0) + 1));
+
+    const tArr = Object.values(tDict).sort((a, b) => b - a);
+
+    for (const t of tArr) {
+        answer++;
+        if (k > t) k -= t;
+        else break;
+    }
+    return answer;
+}
